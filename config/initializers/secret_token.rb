27e85045c7a05a069d8b1f13f8ef6b0bc20c8a88
andpdas <andpdas@gmail.com>
2013-06-09 01:50:52 +0900

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Instamarket::Application.config.secret_token = '1f3f56466b1c3990da0e5c4f09fa2644dda0d66cd0260a931323a09343bad510dcfd4aba5cab78ce6390577c477e8daf17c74094cbebe1660504015011f27085'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Instamarket::Application.config.session_store :cookie_store, key: '_instamarket_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Instamarket::Application.config.session_store :active_record_store
